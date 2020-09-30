979e44bf78180751e58b32911b65e1abbe906464
ZhangHao0810 <874993952@qq.com>
2020-09-30 23:41:47 +0800

    批量sql操作.   数据库的测试完成 , 保证数据库这个部分是完美的. 再做其他.

--- src/main/java/com/zhanghao/crud/test/MapperTest.java
+++ src/main/java/com/zhanghao/crud/test/MapperTest.java
@@ -2,6 +2,9 @@ package com.zhanghao.crud.test;
 
 import static org.hamcrest.CoreMatchers.nullValue;
 
+import java.util.UUID;
+
+import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,7 +33,10 @@ public class MapperTest {
 	DepartmentMapper departmentMapper;
 	
 	@Autowired
-	EmployeeMapper employee;
+	EmployeeMapper employeeMapper;
+	
+	@Autowired
+	SqlSession sqlSession;//生成批量的sqlSession  在applicationContext中配置.
 
 	/**
 	 * 测试DepartmentMapper
@@ -52,10 +58,20 @@ public class MapperTest {
 		
 		
 		//2.生成员工数据 测试员工插入.
-		employee.insertSelective(new Employee(null,"王祺","M","mm王祺@91pron.com",1));
+//		employeeMapper.insertSelective(new Employee(null,"wq","M","mmwq@91pron.com",1));
 		
 		//3.批量插入多个;
 		
-		
+//	    用for循环来做的话, 效能很差.
+//		for() {
+//			employeeMapper.insertSelective(new Employee(null,"wq","M","mmwq@91pron.com",1));
+//		}
+		//批量的插入.
+		EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+		for (int i = 0; i < 1000; i++) {
+			String uid = UUID.randomUUID().toString().substring(0, 5)+i;
+			mapper.insertSelective(new Employee(null,i+":wq","M",uid+":swq@91pron.com",1));
+		}
+		System.out.println("批量删除完成.");
 	}
 }
