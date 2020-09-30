57deb6264bcd2e99ef90ef88543c7aafaa60c7f7
ZhangHao0810 <874993952@qq.com>
2020-09-30 21:25:38 +0800

    12312

--- src/main/java/com/zhanghao/crud/bean/Employee.java
+++ src/main/java/com/zhanghao/crud/bean/Employee.java
@@ -1,17 +1,62 @@
 package com.zhanghao.crud.bean;
+//
+//import javax.validation.constraints.Pattern;
+//
+//import org.hibernate.validator.constraints.Email;
+//import org.hibernate.validator.constraints.Length;
+
 
 public class Employee {
     private Integer empId;
 
+//    @Pattern(regexp="(^[a-zA-Z0-9_-]{6,16}$)|(^[\u2E80-\u9FFF]{2,5})"
+//    		,message="鐢ㄦ埛鍚嶅繀椤绘槸2-5浣嶄腑鏂囨垨鑰�6-16浣嶈嫳鏂囧拰鏁板瓧鐨勭粍鍚�")
     private String empName;
 
     private String gender;
 
+    //@Email
+//    @Pattern(regexp="^([a-z0-9_\\.-]+)@([\\da-z\\.-]+)\\.([a-z\\.]{2,6})$",
+//    		message="閭鏍煎紡涓嶆纭�")
     private String email;
 
     private Integer dId;
+    
+    //甯屾湜鏌ヨ鍛樺伐鐨勫悓鏃堕儴闂ㄤ俊鎭篃鏄煡璇㈠ソ鐨�
+    private Department department;
+    
 
-    public Integer getEmpId() {
+    
+    @Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName
+				+ ", gender=" + gender + ", email=" + email + ", dId=" + dId
+				+ "]";
+	}
+
+	public Employee() {
+		super();
+	}
+
+	public Employee(Integer empId, String empName, String gender, String email,
+			Integer dId) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.gender = gender;
+		this.email = email;
+		this.dId = dId;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public Integer getEmpId() {
         return empId;
     }
 
