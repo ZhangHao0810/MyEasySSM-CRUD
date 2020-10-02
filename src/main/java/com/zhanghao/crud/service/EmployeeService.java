4f3a01040f4d322a66190ff7e2c25ad23f5ae1d3
ZhangHao0810 <874993952@qq.com>
2020-10-02 21:53:36 +0800

    Service层

---
+++ src/main/java/com/zhanghao/crud/service/EmployeeService.java
@@ -0,0 +1,23 @@
+package com.zhanghao.crud.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zhanghao.crud.bean.Employee;
+import com.zhanghao.crud.dao.EmployeeMapper;
+
+@Service
+public class EmployeeService {
+	
+	@Autowired
+	EmployeeMapper employeeMapper;
+
+	//查询所有员工
+	public List<Employee> getAll() {
+		// TODO Auto-generated method stub
+		return employeeMapper.selectByExampleWithDept(null);
+	}
+
+}
