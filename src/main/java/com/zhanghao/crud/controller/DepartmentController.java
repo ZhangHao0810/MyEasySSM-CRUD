714ce6a36194450019b97a68df3540476b4775df
ZhangHao0810 <874993952@qq.com>
2020-10-08 22:33:26 +0800

    Department的controller

---
+++ src/main/java/com/zhanghao/crud/controller/DepartmentController.java
@@ -0,0 +1,39 @@
+package com.zhanghao.crud.controller;
+
+import static org.hamcrest.CoreMatchers.nullValue;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.zhanghao.crud.bean.Department;
+import com.zhanghao.crud.bean.Msg;
+import com.zhanghao.crud.service.DepartmentService;
+
+/**
+ * 	处理和部门有关的请求；	
+ * @author ZhangHao
+ *  2020-10-8
+ */
+@Controller
+public class DepartmentController {
+	
+	@Autowired
+	private DepartmentService  departmentService;
+
+	
+	/**
+	 * 	返回所有的部门信息。
+	 */
+	@RequestMapping("/depts")
+	@ResponseBody
+	public Msg getDepts() {
+		//查出的所有部门信息。
+		List<Department> list =departmentService.getDepts();
+		
+		return Msg.success().add("depts", list);
+	}
+}
