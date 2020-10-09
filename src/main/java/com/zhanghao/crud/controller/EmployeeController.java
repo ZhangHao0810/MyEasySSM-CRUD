c22088893ae22bbf9921891bd543422eda42c2dc
ZhangHao0810 <874993952@qq.com>
2020-10-09 21:53:41 +0800

    员工添加的交互。

--- src/main/java/com/zhanghao/crud/controller/EmployeeController.java
+++ src/main/java/com/zhanghao/crud/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.zhanghao.crud.controller;
 
+import static org.hamcrest.CoreMatchers.nullValue;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,7 +20,12 @@ import com.zhanghao.crud.service.EmployeeService;
 
 /**
  * 处理员工CRUD请求.
- * 
+ * 		URI：
+ * 		/emp/{id} GET请求，查询员工
+ * 		/emp  POST请求，保存员工
+ * 		/emp/{id} PUT请求， 修改员工
+ * 		/emp/{id} DELETE请求， 删除员工。
+ * 	这样只有请求不同的时候，走的controller不同，实现功能不同。这几个功能都是emp开头。
  * @author ZhangHao
  *
  */
@@ -26,6 +34,20 @@ public class EmployeeController {
 
 	@Autowired
 	EmployeeService employeeService;
+	
+	
+	/**
+	 * 	保存员工信息
+	 *   POST 请求。
+	 * 	 @return
+	 */
+	@RequestMapping(value="/emp",method = RequestMethod.POST)
+	@ResponseBody
+	public Msg saveEmp(Employee employee) {
+		employeeService.saveEmp(employee);
+		return Msg.success();
+	}
+	
 
 	/**
 	 * ResponseBody想要能工作, 要导入Jackson依赖.
